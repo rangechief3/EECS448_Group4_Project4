9cc1a63ce7b644c78dc3eea40ef2b7a3ff7b73e7
Mason <mjhawks721@gmail.com>
2021-03-28 20:44:02 -0500

    first pass at mapping out game class using comments and speculative variable names, nothing functional yet

--- Classes/game.py
+++ Classes/game.py
@@ -8,3 +8,65 @@ class Game:
 
     def update(self):
         pygame.display.update()
+    def gameCycle():
+        playerArray = self.updatePlayers();
+        self.deal();
+        gamePhase = 0
+        while(gameInProgress == True)
+        {
+            while(allplayerscall = False)
+            {
+                PlayerWhoRaised = -1;
+                for (i = 0; i< playerArray.length; i++)
+                {
+                    if(PlayerWhoRaised = i)
+                    {
+                        break;
+                    }
+                    #ask player their choice
+                    #if call continue
+                    #if raise then set that player to playerwhoraised and set current bet to their bet
+                    
+                }
+
+                allplayerscall = true;
+                for (i = 0; i< playerArray.length; i++)
+                {
+                    if (playerArray[i].bet != currentbet)
+                    {
+                        allplayerscall = false;
+                    }
+                }
+                
+            }
+            
+            
+            if (gamePhase == 0)
+            {
+                #doflop
+            }
+            else if(gamePhase == 1)
+            {
+                #flipnext card
+            }
+            else if(gamePhase == 2)
+            {
+                #flipnext card
+            }
+            else if(gamePhase == 3)
+            {
+                #decide winner
+                #gameInProgress = false;
+            }
+            gamePhase++
+        }
+        
+    def deal():
+        #give each player 2 cards
+
+    def updatePlayers():
+        #check if any players are wating in queue
+        #check if any players want to leave
+        #return player array
+
+    
