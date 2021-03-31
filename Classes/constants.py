3c33b3d4cf4c4cd3188828e493ae05395dddf9a0
Jake Wagner <jtwagnerkansas@gmail.com>
2021-03-31 17:35:28 -0500

    comments for player.py

--- Classes/constants.py
+++ Classes/constants.py
@@ -7,3 +7,5 @@ HEIGHT = 500
 START_STACK = 1000
 SM_BLIND = 5
 BIG_BLIND = 10
+
+SUITS = ['♣','♦','♠','♥']
--- Classes/player.py
+++ Classes/player.py
@@ -20,16 +20,10 @@ class Player:
     def __str__(self):
         return "Player Name: " + self.player_name + " --- " + "Player Number: " + str(self.player_num)
 
-    def bet(self, curr_bet, prev_bet):
-        if curr_bet == 0:
-            self.stack -= (10 + prev_bet)
-            return 10 - prev_bet
-        self.stack -= (curr_bet + prev_bet)
-        return curr_bet - prev_bet
-        # return 0 for check, curr_bet for call, higher value for raise, -1 for fold
-        # reduce player stack by bet amount then return it
 
-    def blind(self, blind_amt):  # check if the player has that much, if so, return that much
+    def blind(self, blind_amt):  
+        ###same as in bet, determine if the player has that much money left
+        ###if it doesn't then return the maxium amt it cant(subtracting so that self.stack = 0) and the data class will handle
         self.stack -= blind_amt  # if less, return the max they have
         return blind_amt
 
@@ -37,6 +31,10 @@ class Player:
     # @param - A list of cards that will be the objects hand
     # @return - nothing
     def receive_hand(self, hand):
+        ###With that poker module, the cards are actually already in a built-in class(maybe it would have been easier to just implement with numbers idk)
+        ### but it will come in a Card object. To get the rank and suit, they can be accessed with card.suit and card.rank
+        ###  these will return strings for the rank(2-9 or T,J,Q,K,A) and suit. The suits are special characters. I have these in the constants in data.py
+        ### and have added them to this branch 
         i = 0
         print(f'Recieving cards for {self.player_name}')
         for card, tuple in enumerate(hand):
@@ -50,6 +48,9 @@ class Player:
     # @param - A list of numbers that will be the board cards
     # @return - nothing
     def receive_board_cards(self, cards):  # list of cards. WILL be duplicates
+        ###Same thing as above, it will come in as Card objects that can be accessed with Card.rank and card.suit
+        ###Can Not just use append. Check if the card already exists in self.board_cards before using append
+        ### the entire board will be sent each time so there will be duplicates
         i = 0
         print(f"Receiving board cards")
         for card, tuple in enumerate(cards):
@@ -68,7 +69,7 @@ class Player:
     # @description - Draws each card from current players hand
     # @param - nothing
     # @return - nothing
-    def draw_card(self):
+    def draw_cards(self):
         # each card is an object from card class
         self.hand[0].draw()
         self.hand[1].draw()
@@ -76,16 +77,16 @@ class Player:
     # @description - Draws the board cards
     # @param - nothing
     # @return - nothing
-    def draw_board(self):
-        for card in range(len(self.board_cards)):
-            self.board_cards[card].draw()
+    def draw_board_cards(self):
+        for i in range(len(self.board_cards)):
+            self.board_cards[i].draw()
 
     # @description - Draws all the other players cards
     # @param - nothing
     # @return - nothing
     def draw_opponents(self, other_players):
         for player in other_players:
-            player.draw_card()
+            player.draw_cards()
 
     # @description - Current player has folded. Players hand is empty
     # @param - nothing
@@ -94,6 +95,38 @@ class Player:
         self.playing = False
         self.hand = []
 
-    def take_turn(self, cur_bet):
+    def take_turn(self, cur_bet, prev_bet):
         # Might need some help with this method. Played poker like 5 times but have no clue how to play.
+        ###This will be an input loop similar to what happens in main. We will await input from the user in terms of buttons
+
+        ### for btn in self.btns:
+        ###     btn.activate will make it appear, otherwise it will be hidden 
+        ### while not input:
+        ### 
+        ###     for event in pygame.event.get():
+        ###         if event.type == pygame.MOUSEBUTTONDONW
+        ###             check if it hit a button in self.btns - these will include FOLD, CALL, CHECK, RAISE, CONFIRM or 
+        ###                                                     something that can do those functions. The reason for confirm
+        ###                                                     would be so that a person could click raise several times
+        ###             if it should do something, FOLD, CALL, CHECK, etc then do it 
+        ###             and end the loop
+        ###             
+        ###ACTUALLY, JUST GET RID OF BET() AND JUST DO TAKE_TURN 
         pass
+
+    def bet(self, curr_bet, prev_bet):
+        ### This was for testing for me, so change this to be based on user input 
+        ### self.take_turn() 
+        ### keep track of what they do. If they end up calling fold() then return -1, if they bet the same as the current bet, then 
+        ### return curr_bet - prev_bet
+        ### if they raise, then return the value they raised, etc
+        if curr_bet == 0:
+            self.stack -= (10 + prev_bet)
+            return 10 - prev_bet
+        self.stack -= (curr_bet + prev_bet)
+        return curr_bet - prev_bet
+        # return 0 for check, curr_bet for call, higher value for raise, -1 for fold
+        # reduce player stack by bet amount then return it
+
+
+        ###Add drawing of the board and player symbols, etc. 
